ab15cbfa5705a9de65b0e0cb93577752a2b115fa
carrasco21abdulla <carrasco21abdulla@gmail.com>
2024-09-02 11:59:57 +0300

    my first commit for security project

---
+++ src/main/java/com/security/with_security/WithSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.with_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/with_security/WithSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.with_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
