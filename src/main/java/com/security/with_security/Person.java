fd816bcba85267272784d16178bba63132e327b3
carrasco21abdulla <carrasco21abdulla@gmail.com>
2024-09-02 12:10:21 +0300

    add a person class

---
+++ src/main/java/com/security/with_security/Person.java
@@ -0,0 +1,23 @@
+package com.security.with_security;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String name;
+    private String age;
+}
