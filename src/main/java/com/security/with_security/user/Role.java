7f534fa162fbddb5ce5280d12b011d5a39d2d8ee
carrasco21abdulla <carrasco21abdulla@gmail.com>
2024-09-02 13:41:13 +0300

    adding userDetails in user class and creating configuration package with

---
+++ src/main/java/com/security/with_security/user/Role.java
@@ -0,0 +1,7 @@
+package com.security.with_security.user;
+
+public enum Role {
+
+    ADMIN,
+    USER
+}
--- src/main/java/com/security/with_security/user/User.java
+++ src/main/java/com/security/with_security/user/User.java
@@ -4,12 +4,19 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@Table(name = "_user")
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -18,4 +25,43 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
